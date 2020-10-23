3bf1295347e4b9839573fe8b703e70a278d8f7d7
Carlos Alanuca <smsqvaileta@gmail.com>
2020-10-23 16:33:57 -0500

    segunto commit

--- app/src/main/java/com/example/testadmin/Vista/LoginView/VistaLogin.java
+++ app/src/main/java/com/example/testadmin/Vista/LoginView/VistaLogin.java
@@ -1,8 +1,10 @@
-package com.example.testadmin;
+package com.example.testadmin.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.testadmin.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/testadmin/Vista/PrincipalView/VistaPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.testadmin.Vista.PrincipalView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VistaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_principal);
+    }
+}
